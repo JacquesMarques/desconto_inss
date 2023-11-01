efb3c166420b5c28f0f52bd372cf2a7f64d17a5c
Jacques Marques <jmarques1@gmail.com>
2023-11-01 11:01:15 -0300

    feature: create action in home controller for calculation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
+  end
+
+  def calcule_inss_discount
+    CalculateInssDiscountJob.perform_later
+
+    redirect_to root_path, notice: 'Cáclulo de Desconto de INSS solicitado. Aguarde...'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  get 'home/calcule_inss_discount', to: 'home#calcule_inss_discount'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 end
