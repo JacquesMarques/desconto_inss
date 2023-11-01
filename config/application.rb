f0aa7e4fcadd55549ff493a12e6226959ade1038
Jacques Marques <jmarques1@gmail.com>
2023-11-01 10:16:50 -0300

    fix: configure pt-BR locale

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,10 @@ module DescontoInss
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = %i[pt-BR en]
+    config.time_zone = 'Brasilia'
   end
 end
