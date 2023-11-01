800128e1b0b1f3d53a89dbd2b57fd5aefc4aa7a6
Jacques Marques <jmarques1@gmail.com>
2023-11-01 11:21:49 -0300

    feature: setup authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    return if current_user.nil?
+
+    @current_user = current_user
+  end
 end
